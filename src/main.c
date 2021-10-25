d347da55ece78e4a97d44c5f98c9fd390f1475ee
Ivano Bilenchi <ivanobilenchi@gmail.com>
2021-10-25 16:16:07 +0200

    Improve error handling

--- src/main.c
+++ src/main.c
@@ -4,22 +4,27 @@
 #include "parser.h"
 
 int main(void) {
-    char expr[256];
-    int ret;
-
     while (1) {
         fflush(stdin);
+        char expr[256];
 
         if (!fgets(expr, sizeof(expr), stdin)) {
             continue;
         }
 
-        YY_BUFFER_STATE state = yy_scan_bytes(expr, strcspn(expr, "\n"));
+        YY_BUFFER_STATE state;
 
-        if (state && yyparse(&ret) == 0) {
-            printf("= %d\n", ret);
-            yy_delete_buffer(state);
+        if (!(state = yy_scan_bytes(expr, strcspn(expr, "\n")))) {
+            continue;
         }
+
+        int ret;
+
+        if (yyparse(&ret) == 0) {
+            printf("= %d\n", ret);
+        }
+
+        yy_delete_buffer(state);
     }
 
     return 0;
